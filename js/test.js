e14a9311759a98f936260c5401f8a409d53e3362
simon <simon.bock@liip.ch>
2019-04-08 14:10:04 +0200

    game fertig

--- js/test.js
+++ js/test.js
@@ -1,4 +1,4 @@
-// "use strict";
+//"use strict";
 
 // function romanNumerals(number) {
 //     let testdiff = 0;
@@ -69,36 +69,51 @@
 // }
 /** Push Funktion von Arr erweiter: (arr._push)  diese pushed und überprüft*/
 // //showWindowContent(window);
+
+/**
+ * Tic Tac Toe
+ */
 let playerIcon = "X";
 let arr = [];
 
 const player = document.querySelector(".player");
+/**
+ * Getting all fields and add a click listener.
+ * if the field is pressed once the listener is getting removed.
+ */
 document.querySelectorAll(".row td").forEach((element, index) => {
     element.addEventListener('click', function () {
-
         if(arr.length) {
             let lastArrayElement = arr.length-1;
             playerIcon = arr[lastArrayElement].value == "X" ? "O" : "X"
-            
         }
         arr._push(index, playerIcon);
         element.firstChild.innerText = playerIcon;
-
+        player.innerText = player.innerText == "X" ? "O" : "X"
         element.removeEventListener('click', arguments.callee);
         console.log("arr", {arr})
     });
 });
 
+
 Array.prototype._push = function(index, value) {
     let winnArr = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
     this.push({position: index, value: value})
-
     if(this.length >= 5) {
         if(matchingPosition(arr.filter(function(item) {return item.value == "X"}), winnArr)) {
-            console.log("X is Winner");
+            // window.requestAnimationFrame(function () {
+            //     alert("X is Winner")
+            // });
+            window.setTimeout(function () {
+                alert("X is Winner");
+                clearField();
+            }, 0);
         }
         if(matchingPosition(arr.filter(function(item) {return item.value == "O"}), winnArr)) {
-            console.log("O is Winner");
+            window.setTimeout(function () {
+                alert("O is Winner");
+                clearField();
+            }, 0);
         }
     }
 }
@@ -118,18 +133,17 @@ function matchingPosition(resultArr, winArr) {
                 if (counter === 3) {
                     return true
                 }
-
             } else {
                 if(counter > 0) {
                     counter -= 1;
                     console.log("False: " + counter)
                 }
-                
-                
             }
-        }
-        
+        } 
     }
-    
-
+}
+function clearField() {
+    document.querySelectorAll(".row td").forEach((element, index) => {
+        element.innerText = "";
+    }); 
 }
