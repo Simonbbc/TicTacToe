cfeef3ebd09958cbaa027bcecff72235deea81f8
simon <simon.bock@liip.ch>
2019-04-10 15:47:58 +0200

    add comments in js

--- js/test.js
+++ js/test.js
@@ -1,75 +1,3 @@
-//"use strict";
-
-// function romanNumerals(number) {
-//     let testdiff = 0;
-//     let testArr = [];
-//     let resultString = "";
-//     var romanNumerals = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1}
-//     // minus rechnen nicht plus. Bis die Jeweilige zahl nicht mehr minus gerechnet werden kann.
-
-//     for (let index in romanNumerals) {
-//         // alert(index);
-//         alert(romanNumerals[index]);
-//         while (number >= romanNumerals[index]) {
-//             number -= romanNumerals[index];
-//             resultString += index;
-
-//             alert("new Number: " + number);
-//         }
-//     } 
-//     alert(resultString);
-// }
-
-// //romanNumerals(6) // == 'VI'
-// //romanNumerals(76) // == 'LXXVI'
-// //romanNumerals(13) // == 'XIII'
-// //romanNumerals(44) // == 'XLIV'
-// //romanNumerals(3999) // == 'MMMCMXCIX'
-
-// function secondIndex(string, index) {
-//     let position = 0;
-//     for (let i = 0; i < string.length; i++){
-//         if (index != " " || index != ""){
-//             alert("String: " + string[i]);
-//             if (string[i] == index) {
-//                 if (position != 0){
-//                     if (position < i){
-//                         alert("Result: " + i);
-//                         break;
-//                     } else {
-//                         alert("Result: " + position);
-//                         break;
-//                     }
-//                 }
-//                 position = i + 1;
-//                 alert("Position: " + position)
-                
-//             }
-//         }
-//         alert(undefined);
-//     }
-// }
-
-// //secondIndex("sims", "s") == 3
-// //secondIndex("find the river", "e") == 12
-// secondIndex("hi", " ")  == undefined
-
-
-// function showDate() {
-//     let fullDate = new Date();
-//     let daylist = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag']
-//     console.log("Heute ist: " + daylist[fullDate.getDate() - 1]);
-//     console.log("Die aktuelle Zeit ist: " + fullDate.getHours() + ":" + fullDate.getMinutes() + ":" + fullDate.getSeconds() + " Uhr");
-    
-// }
-// //showDate();
-
-// function showWindowContent(currentWindow) {
-//     currentWindow.print();
-// }
-/** Push Funktion von Arr erweiter: (arr._push)  diese pushed und überprüft*/
-// //showWindowContent(window);
-
 /**
  * Tic Tac Toe
  */
@@ -78,24 +6,39 @@ let arr = [];
 
 const player = document.querySelector(".player");
 /**
- * Getting all fields and add a click listener.
- * if the field is pressed once the listener is getting removed.
+ * Zuerst holen wir uns alle Felder vom TicTacToe.
+ * Danach binden wir einen Click Listener an jedes Feld.
+ * Wenn das Feldarray eine länge besitzt wird die Variable playerIcon auf den Wert gesetzt,
+ * der in dem Array ist.
+ * Danach wird eine funktion auf dem Array ausgeführt _push, dies ist eine Custom Prototype Function und dieser geben wir den index vom Feld und das Player Icon mit.
+ * Danach wird dem Feld der wert von der Variable playerIcon gegeben.
+ * Der Player wird danach getauscht.
+ * und das Feld verliert den Click Listener, damit es nicht 2 Mal gedrückt werden kann.
  */
-document.querySelectorAll(".row td").forEach((element, index) => {
-    element.addEventListener('click', function () {
+document.querySelectorAll(".row td").forEach((field, index) => {
+    field.addEventListener('click', function () {
         if(arr.length) {
             let lastArrayElement = arr.length-1;
             playerIcon = arr[lastArrayElement].value == "X" ? "O" : "X"
         }
         arr._push(index, playerIcon);
-        element.firstChild.innerText = playerIcon;
+        field.firstChild.innerText = playerIcon;
         player.innerText = player.innerText == "X" ? "O" : "X"
-        element.removeEventListener('click', arguments.callee);
-        console.log("arr", {arr})
+        field.removeEventListener('click', arguments.callee);
     });
 });
 
-
+/**
+ * Zuerst geben wird dem Standart Array Objekt eine neue Prototype Function namens _push und verlangen die Parameter index und value.
+ * Dann wird ein Array aus kleinen Arrays gemacht um eine Winningcondition zu haben.
+ * Danach wird auf dem Standart Array Objekt die Funktion push ausgeführt.
+ * Danach wird geschaut ob das Array die Länge 5 hat, weil man erst ab 5 mal drücken frühstens gewinnen kann.
+ * Danach wird geprüft, indem die Funktion matchingPosition aufgerufen wird, welche somit true oder false zurück gibt.
+ * Dieser Funktion geben wir das Array mit jedoch nur Einträge welche die value X haben.
+ * Wenn dies zutrifft wird der Gewinner X sein und das Spielfeld wird geleert.
+ * Wenn das vorherige nicht zutrifft geben wir der gleichen Funktion das Array, nur mit Einträgen mit der value O, mit.
+ * Wenn dass zutrifft ist der gewinner O und das Spielfeld wird geleert.
+ */
 Array.prototype._push = function(index, value) {
     let winnArr = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
     this.push({position: index, value: value})
@@ -118,31 +61,35 @@ Array.prototype._push = function(index, value) {
         
     }
 }
-
+/**
+ * Dieser Funktion werden die Parameter resultArr und winArr mitgegeben.
+ * Danach wird das resultArr auf die Anforderungen angepasst durch die map Funktion von Javascript.
+ * Danach wird geschaut ob alle X auf dem Spielfeld eine 3 Kombination aus dem winArr matchen.
+ * Wenn ja wird true zurück gegeben, ansonsten wird false zurück gegeben.
+ */
 function matchingPosition(resultArr, winArr) {
     let counter = 0;
     let cleanedResultArr = resultArr.map(item => {
         return item.position
     })
     for(let i = 0; i < winArr.length; i++){
-        console.log(cleanedResultArr);
-        console.log(winArr[i]);
         for(let j = 0; j < resultArr.length; j++){
             if(cleanedResultArr.some(r=> winArr[i][j] === r)) {
                 counter += 1;
-                console.log("True", counter)
                 if (counter === 3) {
                     return true
                 }
             } else {
                 if(counter > 0) {
                     counter -= 1;
-                    console.log("False: " + counter)
                 }
             }
         } 
     }
 }
+/**
+ * Diese Funktion holt sich alle Felder und setzt deren Text auf nichts.
+ */
 function clearField() {
     document.querySelectorAll(".row td").forEach((element, index) => {
         element.innerText = "";
